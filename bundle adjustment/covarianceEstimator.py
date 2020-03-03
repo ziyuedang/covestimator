68fc374871dc3babf5f84b9ef56d143d0351eee0
Dang <zdang2@CougarNet.UH.EDU>
2020-03-03 17:00:59 -0600

    update

--- bundle adjustment/covarianceEstimator.py
+++ bundle adjustment/covarianceEstimator.py
@@ -10,50 +10,60 @@ compute covariance for parameters and
 import sfm_IO as IO
 import numpy as np
 import math as m
-from collections import defaultdict 
 from scipy.linalg import inv
 from numpy.linalg import multi_dot
 
 
-root_path, extrinsics, intrinsics, coords_3d, views_meta, control_points = read_sfm('sfm.json', 'D:/SFM/covestimator/Data/')
+root_path, extrinsics, intrinsics, coords_3d, views_meta, control_points = IO.read_sfm('sfm.json', 'D:/SFM/covestimator/Data/')
 
-imgID = []
-img_obs = []
-XYZ_3D = []
-
-for i in range(0, len(coords_3d)):
-    coords_3d[i]['index_3D'] = i
-    
-for i in range(0, len(coords_3d)):
-    pnt_3d = coords_3d[i]['observations']
-    index_3d = coords_3d[i]['index_3D']  
-    for img in pnt_3d:
-        img.update({"index_3D": index_3d})
-    img_obs.extend(pnt_3d)
-
-n_imgs = len(views_meta)
-obs_count = []
-
-for i in range(0, n_imgs):
-    obs_count.append(imgID.count(i))
-
-# list of 2-D image observations for all images
-    
-merged_obs = []
-for i in range(0, n_imgs):
-    temp = []
-    for d in range(0, len(img_obs)):
-        keyID = img_obs[d]['key']
-        if keyID == i:
-            temp.append(img_obs[d]['value']['x'])
-            temp_array = np.array(temp)
-    merged_obs.append(temp)
 
 # 3-D coordinates corresponding to 2-D image observations
 
 
-def initParams(obs_count, extrinsics, intrinsics, n_imgs):
-    
+def initParams(views_meta, coords_3d, extrinsics, intrinsics):
+    """
+    This function initializes bundle adjustment parameters and data inputs.
+    Returns: Design matrices Ae, Ao, EO initial values (XYZc, rotation)
+    2D image observations (merged_obs), 3D object point coordinates (XYZ_3D)
+    """
+    img_obs = []
+    n_imgs = len(views_meta)
+
+    # XYZ_3D - flattened 3D object point coordinates
+    XYZ_3D = np.empty((0, 0))
+    for i in range(0, len(coords_3d)):
+        coords_3d[i]['index_3D'] = i
+        XYZ_3D = np.append(XYZ_3D, np.array(coords_3d[i]['X']))
+
+    for i in range(0, len(coords_3d)):
+        pnt_3d = coords_3d[i]['observations']
+        index_3d = coords_3d[i]['index_3D']  
+        for img in pnt_3d:
+            img.update({"index_3D": index_3d})
+        img_obs.extend(pnt_3d)
+
+
+    # list of 2-D image observations for all images
+    # merged_obs is a list with length of (n_imgs), each value for each image
+    # corresponds to number of observations for that image, (x, y, index of the 3D
+    # point which can be found in coords_3d)
+    merged_obs = []
+    obs_count = []
+    for i in range(0, n_imgs):
+        temp = []
+        for d in range(0, len(img_obs)):
+            a = []
+            b = []
+            keyID = img_obs[d]['key']
+            if keyID == i:
+                a = img_obs[d]['value']['x']
+                b = img_obs[d]['index_3D']
+                a.append(b)
+                temp.append(a)
+        obs_count.append(len(temp))
+        merged_obs.append(temp)
+
+
     # Number of 2D image observations
     n = 2 * sum(obs_count) 
     
@@ -78,10 +88,45 @@ def initParams(obs_count, extrinsics, intrinsics, n_imgs):
     XYZc = []
     rotation = []
     for i in range(0, n_imgs):
-        XYZc[i] = extrinsics[i]['center']
-        rotation[i] = extrinsics[i]['rotation']
-        
+        XYZc.append(extrinsics[i]['center'])
+        rotation.append(extrinsics[i]['rotation'])
     
+    # Intrinsics unpack
+    interior = intrinsics['ptr_wrapper']['data']
+        
+    return Ae, Ao, dxe, dxo, XYZc, rotation, merged_obs, XYZ_3D, interior
+
+def apply_distortion(interior, transformed_3D_point):
+    k1, k2, k3 = interior['disto_k3']
+    c = interior['focal_length']
+    xp, yp = interior['principal_point']
+    
+    # 3D homogeneous -> 2D euclidean
+    projected_point = transformed_3D_point[0:2] / transformed_3D_point[-1]
+    
+    # distortion
+    r2 = np.inner(projected_point, projected_point)
+    r4 = r2 * r2
+    r6 = r4 * r2
+    r_coeff = (1 + k1 * r2 + k2 * r4 + k3 * r6)
+    return xp, yp, projected_point, r_coeff, c
+    
+        
+def cost_function(intrinsics, R, XYZc, XYZ_3D, interior):
+    """
+    This evaluates the image x, y based on collinearity.
+    Input: camera parameters, exterior orientation parameters, points' coordinates
+    in real world frame.
+    Return: x, y on the image (pixel)
+    """    
+    transformed_3D_point = np.dot(R, XYZ_3D) + XYZc
+    xp, yp, projected_point, r_coeff, c = apply_distortion(interior, transformed_3D_point)
+    
+    # collinearity equations
+    # x = xp - c*(m11*(X - Xc) + m12*(Y - Yc) + m13*(Z - Zc))/(m31*(X - Xc) + m32*(Y - Yc) + m33*(Z - Zc))
+    # y = yp - c*(m21*(X - Xc) + m22*(Y - Yc) + m23*(Z - Zc))/(m31*(X - Xc) + m32*(Y - Yc) + m33*(Z - Zc))
+    
+    return x, y    
         
 
     
